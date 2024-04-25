43abfa93c6aa6868e73528f06b78002f1ad44731
Livanhernandez <livanhernandez9@gmail.com>
2024-04-25 18:45:30 -0400

    updated

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python3
-"""
-Take the code from wait_n and alter it into a new function task_wait_n. The code is nearly identical to wait_n except task_wait_random is being called.
-"""
+'''
+    Function task_wait_n that takes in an integer
+    n and max_delay and returns a asyncio.Task.
+'''
 from typing import List
 wait_n = __import__('1-concurrent_coroutines').wait_n
 
+
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
-    """
-    Return a list of floats of length n.
-    """
+    '''
+        Function that takes in an integer n and
+        max_delay and returns a asyncio.Task.
+    '''
     return await wait_n(n, max_delay)
