775305c8f7e4f1b0654d4bb16e7c20b2ac61262d
Livanhernandez <livanhernandez9@gmail.com>
2024-04-25 14:59:46 -0400

    added imports

--- python_variable_annotations/6-sum_mixed_list.py
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -4,8 +4,10 @@ Write a type-annotated function sum_mixed_list
 which takes a list mxd_lst of integers and floats
 and returns their sum as a float.
 """
+from typing import List
+from typing import Union
 
 
-def sum_mixed_list(mxd_lst: list) -> float:
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     """Return the sum of a list of integers and floats."""
-    return sum(mxd_lst)
+    return float(sum(mxd_lst))
