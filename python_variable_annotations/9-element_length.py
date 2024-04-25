1ecfb953679f57b934a1f45699cb2f14567e13c4
Livanhernandez <livanhernandez9@gmail.com>
2024-04-25 15:27:56 -0400

    fixed

--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -1,15 +1,24 @@
 #!/usr/bin/env python3
-"""
-Function element_lenght hat takes a list
+'''
+    Function element_length that takes a list
     lst of strings as argument and returns a list
     of integers representing the lengths of the
     corresponding strings
-"""
-from typing import Iterable, List, Tuple
+'''
+from typing import Iterable, List, Sequence, Tuple
 
 
-def element_lenght(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
-    """
-    Function element_lenght
-    """
+# type-annotated function
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    '''
+        Returns a list of tuples containing the
+        elements of lst and their lengths
+
+
+        lst: list of strings
+
+
+        return: list of tuples containing the elements
+                of lst and their lengths
+    '''
     return [(i, len(i)) for i in lst]
