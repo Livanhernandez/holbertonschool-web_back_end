68932e8a8ea8326a2490438cd53e57d4d2c73cb3
Livanhernandez <livanhernandez9@gmail.com>
2024-05-08 12:52:40 -0400

    Add - Added file with changes implemented

--- ES6_classes/3-currency.js
+++ ES6_classes/3-currency.js
@@ -1,26 +1,41 @@
 export default class Currency {
   constructor(code, name) {
-    this._code = code;
-    this._name = name;
+    // Check if currency is a string
+    if (typeof code === 'string') {
+      this.code = code;
+    } else {
+      throw new TypeError('Currency must be a string');
+    }
+
+    // Check if value is a number
+    if (typeof name === 'string') {
+      this.name = name;
+    } else {
+      throw new TypeError('Value must be a string');
+    }
   }
 
+  // currency getter and setter
   get code() {
     return this._code;
   }
 
-  set code(newCode) {
-    this._code = newCode;
+  set code(value) {
+    this._code = value;
   }
 
+  // value getter and setter
   get name() {
     return this._name;
   }
 
-  set name(newName) {
-    this._name = newName;
+  set name(value) {
+    this._name = value;
   }
 
+  // displayFullCurrency method
   displayFullCurrency() {
-    return `${this._code} - ${this._name}`;
+    // Return the value and currency in the following format: value (currency)
+    return `${this._name} (${this._code})`;
   }
 }
