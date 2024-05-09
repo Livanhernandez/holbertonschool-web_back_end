25736f0e3206276d2ce7b359ab893176a6b80e13
Livanhernandez <livanhernandez9@gmail.com>
2024-05-09 11:21:44 -0400

    fixed file

--- ES6_classes/7-airport.js
+++ ES6_classes/7-airport.js
@@ -1,21 +1,19 @@
 export default class Airport {
-    constructor(name, code) {
-        if (typeof name === 'string') {
-            this.name = name;
-        }
-        else {
-            throw new Error('name must be a string');
-        }
+  constructor(name, code) {
+    if (typeof name === 'string') {
+      this._name = name;
+    } else {
+      throw new Error('Name must be a string');
+    }
 
-        if (typeof code ==='string') {
-            this.code = code;
-        }
-        else {
-            throw new Error('code must be a string');
-        }
+    if (typeof code === 'string') {
+      this._code = code;
+    } else {
+      throw new Error('Code must be a string');
     }
-    
-    get [Symbol.toStringTag]() {
-        return `${this.code}`;
-    }
+  }
+
+  get [Symbol.toStringTag]() {
+    return `${this._code}`;
+  }
 }
