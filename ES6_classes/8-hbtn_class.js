3efbc3bf7276e855f2883f2a06de393b4155494d
Livanhernandez <livanhernandez9@gmail.com>
2024-05-09 11:34:20 -0400

    fixed lint

--- ES6_classes/8-hbtn_class.js
+++ ES6_classes/8-hbtn_class.js
@@ -1,22 +1,20 @@
 export default class HolbertonClass {
-    constructor(size, location) {
-      if (typeof size === 'number') {
-        this._size = size;
-      }
-      else {
-        throw new Error('size must be a number');
-      }
-
-      if (typeof location === 'string') {
-        this._location = location;
-      }
-      else {
-        throw new Error('location must be a string');
-      }
+  constructor(size, location) {
+    if (typeof size === 'number') {
+      this._size = size;
+    } else {
+      throw new Error('size must be a number');
     }
-  
-    [Symbol.toPrimitive](type) {
-      if (type === 'string') return this._location;
-      return this._size;
+
+    if (typeof location === 'string') {
+      this._location = location;
+    } else {
+      throw new Error('location must be a string');
     }
   }
+
+  [Symbol.toPrimitive](type) {
+    if (type === 'string') return this._location;
+    return this._size;
+  }
+}
