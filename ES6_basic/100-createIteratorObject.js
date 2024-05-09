d6c8a1348d70b8362b6667f1c7a4f6282775adcc
Livanhernandez <livanhernandez9@gmail.com>
2024-05-09 12:46:42 -0400

    renamed

--- ES6_basic/100-createIteratorObject.js
+++ ES6_basic/100-createIteratorObject.js
@@ -1,5 +1,6 @@
 export default function createIteratorObject(report) {
   const arrayOfEmployees = [];
+  // .allEmployees is an array of employee names
   for (const employees of Object.values(report.allEmployees)) {
     arrayOfEmployees.push(...employees);
   }
